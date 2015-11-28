e91b964ba4417b5cd15580d319a4acaac6ddd784
YiMing <yiming.han@mail.utoronto.ca>
2015-11-27 23:16:07 -0500

    update bookSchema and Posting/Comment System

--- model/books.js
+++ model/books.js
@@ -1,16 +1,25 @@
 var mongoose = require('mongoose');
 var bookSchema = new mongoose.Schema({
     title: String,
-    description: String,
     course: String,
-    owner: String,
-	borrower: String,
-	avaiableTime : String,
-	avaiable: Boolean,
-	ISBN: String,
-	ratecount : Number,
+    ISBN: String,
+    ratecount : Number,
     rating: Number});
 	
+var postingSchema = new mongoose.Schema({
+    ISBN : String,
+    owner : String,
+    description : String,
+    postingname : String
+});
 
+var ratingSchema = new mongoose.Schema({
+    ISBN : String,
+    rater : String,
+    comment : String,
+    rating : Number
+});
 
+mongoose.model('Rating', ratingSchema);
+mongoose.model('Postings', postingSchema);
 mongoose.model('Books',bookSchema);
