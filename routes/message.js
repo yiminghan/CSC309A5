5b24fea1d843202c2086f3e816d18bd5fa82e40f
YiMing <yiming.han@mail.utoronto.ca>
2015-11-29 20:54:20 -0500

    read routes\message_README for usage

--- routes/message.js
+++ routes/message.js
@@ -10,18 +10,19 @@ router.route('/new/:id')
 	//check authentication
 	var data = {authenticated : "false"};
 	if (req.isAuthenticated()){
-	    data = authenticated : "true", user:req.user};
-	var isSameUser = (data.user._id == req.param('id'));
-	if(!isSameUser){
-	    res.json({success: 0 , message: "You do not have permission to see these messages!"});
-	} else { //isSameUser
-	    mongoose.model('Message').find({receiverId:data.user._id, read : 0}).sort({date: -1}).exec(function (err, messages){
-		if (err) {
-		    res.json({success : 0, message:"There was an error retreiving from the database: " + err});
-		} else {
-		    res.json({success : 1, messages : messages});
-		}
-	    });
+	    data = {authenticated : "true", user:req.user};
+	    var isSameUser = (data.user._id == req.param('id'));
+	    if(!isSameUser){
+		res.json({success: 0 , message: "You do not have permission to see these messages!"});
+	    } else { //isSameUser
+		mongoose.model('Message').find({receiverId:data.user._id, read : 0}).sort({date: -1}).exec(function (err, messages){
+		    if (err) {
+			res.json({success : 0, message:"There was an error retreiving from the database: " + err});
+		    } else {
+			res.json({success : 1, messages : messages});
+		    }
+		});
+	    }
 	}
     })
     .post(function (req, res) { //create a new message for :id to receive
@@ -65,20 +66,55 @@ router.route('/new/:id')
 	}
     });
 
-router.route('/sent/:id')
+router.route('/sent')
     .get(function (req, res) {
 	var data = {authenticated : "false"};
 	if (req.isAuthenticated()) {
 	    data = {authenticated : "true", user : req.user};
-	}
+	}q
 	if (data.authenticated == "false"){
-	    res.json({sucess : 0 , message : "You have to login first to check your messages"});
+	    res.json({success : 0 , message : "You have to login first to check your messages"});
 	} else {//authenticated == "true"
 	    mongoose.model('Message').find({messengerId:data.user._id}).sort({date: -1}).exec(function (err, messages){
 		if (err) {
 		    res.json({success : 0, message:"There was an error retreiving from the database: " + err});
 		} else {
 		    res.json({success : 1, messages : messages});
+		}
+	    });
+	}
+    });
+
+router.route('/update/')
+    .get(function (req, res, next) {
+	var data = {authenticated : "false"};
+	if (req.isAuthenticated()) {
+	    data = {authenticated : "true", user : req.user};
+	}
+	if (data.authenticated == "false"){
+	    res.json({success : 0, message: "You have to login first to check messages"});
+	} else {//authenticated == "true"
+	    mongoose.model('Message').count({receiverId : req.user._id, read : 0}, function (err, count){
+		if (err) {
+		    res.json({success:0});
+		} else {
+		    res.json({success : 1, count:count});
+		}
+	    });
+	}
+    });
+
+router.route('/update/:id')
+    .post(function (req, res, next) {
+	var data = {authenticated : "false"};
+	if (req.isAuthenticated()) {
+	    data = {authenticated : "true", user : req.user};
+	    mongoose.model('Message').findById( req.param('id'), function (err, post){
+		if(post.receiverId = data.user._id) {
+		    var readcount = post.read + 1;
+		    post.update({
+			read : readcount
+		    });
 		}
 	    });
 	}
