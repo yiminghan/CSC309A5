eca6b6ca0ec86349fc89ad06128a780873344f1e
YiMing <yiming.han@mail.utoronto.ca>
2015-12-05 17:40:33 -0500

    add some security stuff

--- routes/REST-API/message.js
+++ routes/REST-API/message.js
@@ -6,7 +6,8 @@ var sanitizer = require('sanitizer');
 router.get("/messages", function(req, res){
 	
 	//If there is a query string indicating who the receiver is
-	//Find all messages where the receiver matches the query string
+    //Find all messages where the receiver matches the query string
+    if(req.isAuthenticated()){
 	if (req.query.receiverID){
 		Message.find({receiverID:req.query.receiverID}, function(err, messages){
 			if (err){
@@ -22,74 +23,87 @@ router.get("/messages", function(req, res){
 	        res.json(messages);
 		});
 	}
+    } else {
+	//send error
+    }
 });
 
 router.get("/messages/:id", function(req, res){
-	Message.findById(req.params.id, function(err, message){
-        if (err)
-            res.send(err);
-        res.json(message);
-    })
+    if (req.isAuthenticated()) {
+	if( req.user._id == req.params.id) {
+	    Message.findById(req.params.id, function(err, message){
+		if (err)
+		    res.send(err);
+		res.json(message);
+	    })
+	}
+    }
 });
 
 router.post("/messages", validateFields, function(req, res){
-	var newMessage = new Message();
-	if (req.body.messengerID){
-		newMessage.messengerID = req.body.messengerID;
-	}
-	if (req.body.messengerName){
-		newMessage.messengerName = req.body.messengerName;
-	}
-	if (req.body.receiverID){
-		newMessage.receiverID = req.body.receiverID;
-	}
-	if (req.body.receiverName){
-		newMessage.receiverName = req.body.receiverName;
-	}
-	if (req.body.message){
-		newMessage.message = req.body.message;
-	}
-	if (req.body.read){
-		newMessage.read = req.body.read;
-	}
-	if (req.body.dateSent){
-		newMessage.dateSent = req.body.dateSent;
-	}
-
-	newMessage.save(function(err, message){
-		if (err)
-			res.send(err)
-		res.json(message);
-	})
+    if( req.isAuthenticated()){
+    var newMessage = new Message();
+      //newMessage.messengerID = req.body.messengerID;
+    newMessage.messengerID = req.user._id;
+    if (req.body.messengerName){
+	newMessage.messengerName = req.body.messengerName;
+    }
+    if (req.body.receiverID){
+	newMessage.receiverID = req.body.receiverID;
+    }
+    if (req.body.receiverName){
+	newMessage.receiverName = req.body.receiverName;
+    }
+    if (req.body.message){
+	newMessage.message = req.body.message;
+    }
+    if (req.body.read){
+	newMessage.read = req.body.read;
+    }
+    if (req.body.dateSent){
+	newMessage.dateSent = req.body.dateSent;
+    }
+    
+    newMessage.save(function(err, message){
+	if (err)
+	    res.send(err)
+	res.json(message);
+    });
+    }
 });
+
+
 
 router.delete("/messages/:id", function(req, res){
+    if( req.isAuthentciated())
+    {
 	Message.remove({_id:req.params.id}, function(err, posting){
-        if (err)
-            res.send(err);
-    });
+            if (err)
+		res.send(err);
+	});
+    }
 });
 
-// router.route('/new/:id')
-//     .get(function (req, res) {//get unread mesasges from a certain user
-// 	//check authentication
-// 	var data = {authenticated : "false"};
-// 	if (req.isAuthenticated()){
-// 	    data = {authenticated : "true", user:req.user};
-// 	    var isSameUser = (data.user._id == req.param('id'));
-// 	    if(!isSameUser){
-// 		res.json({success: 0 , message: "You do not have permission to see these messages!"});
-// 	    } else { //isSameUser
-// 		mongoose.model('Message').find({receiverId:data.user._id, read : 0}).sort({date: -1}).exec(function (err, messages){
-// 		    if (err) {
-// 			res.json({success : 0, message:"There was an error retreiving from the database: " + err});
-// 		    } else {
-// 			res.json({success : 1, messages : messages});
-// 		    }
-// 		});
-// 	    }
-// 	}
-//     })
+ router.route('/messages/new/:id')
+     .get(function (req, res) {//get unread mesasges from a certain user
+	//check authentication
+	var data = {authenticated : "false"};
+ 	if (req.isAuthenticated()){ 
+	    data = {authenticated : "true", user:req.user};
+	    var isSameUser = (data.user._id == req.params.id);
+	    if(!isSameUser){
+		res.json({success: 0 , message: "You do not have permission to see these messages!"});
+	    } else { //isSameUser
+		mongoose.model('Message').find({receiverID:data.user._id, read : 0}).sort({date: -1}).exec(function (err, messages){
+		    if (err) {
+ 			res.json({success : 0, message:"There was an error retreiving from the database: " + err});
+		    } else {
+			res.json({success : 1, messages : messages});
+		    }
+ 		});
+	    }
+	}
+     });
 //     .post(function (req, res) { //create a new message for :id to receive
 // 	var data = {authenticated : "false"};
 // 	if (req.isAuthenticated()) {
@@ -150,40 +164,41 @@ router.delete("/messages/:id", function(req, res){
 // 	}
 //     });
 
-// router.route('/update/')
-//     .get(function (req, res, next) {
-// 	var data = {authenticated : "false"};
-// 	if (req.isAuthenticated()) {
-// 	    data = {authenticated : "true", user : req.user};
-// 	}
-// 	if (data.authenticated == "false"){
-// 	    res.json({success : 0, message: "You have to login first to check messages"});
-// 	} else {//authenticated == "true"
-// 	    mongoose.model('Message').count({receiverId : req.user._id, read : 0}, function (err, count){
-// 		if (err) {
-// 		    res.json({success:0});
-// 		} else {
-// 		    res.json({success : 1, count:count});
-// 		}
-// 	    });
-// 	}
-//     });
+router.route('/message/update/')
+     .get(function (req, res, next) {
+ 	var data = {authenticated : "false"};
+	if (req.isAuthenticated()) {
+	    data = {authenticated : "true", user : req.user};
+ 	}
+ 	if (data.authenticated == "false"){
+ 	    res.json({success : 0, message: "You have to login first to check messages"});
+ 	} else {//authenticated == "true"
+	    mongoose.model('Message').count({receiverId : req.user._id, read : false}, function (err, count){
+		if (err) {
+ 		    res.json({success:0});
+ 		} else {
+ 		    res.json({success : 1, count:count});
+ 		}
+ 	    });
+ 	}
+     });
 
-// router.route('/update/:id')
-//     .post(function (req, res, next) {
-// 	var data = {authenticated : "false"};
-// 	if (req.isAuthenticated()) {
-// 	    data = {authenticated : "true", user : req.user};
-// 	    mongoose.model('Message').findById( req.param('id'), function (err, post){
-// 		if(post.receiverId = data.user._id) {
-// 		    var readcount = post.read + 1;
-// 		    post.update({
-// 			read : readcount
-// 		    });
-// 		}
-// 	    });
-// 	}
-//     });
+router.route('/messages/update/:id')
+  .post(function (req, res, next) {
+ 	var data = {authenticated : "false"};
+	if (req.isAuthenticated()) {
+	    data = {authenticated : "true", user : req.user};
+	    mongoose.model('Message').findById( req.param('id'), function (err, post){
+		if(post.receiverID = data.user._id) {
+		    var read = true;
+ 		    post.update({
+ 			read : read
+ 		    });
+ 		}
+	    });
+	}
+  })
+;
 module.exports = router;
 
 
