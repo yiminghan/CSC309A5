ad4dffb5bf4aee0498e100742be830e25a23e79e
Wilbur Hsu <hsuweibo@yahoo.com>
2015-12-06 21:12:30 -0500

    Changes auth.js, update Google Api Key

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@
 //Google's OAuth functionalities
 module.exports = {
     'googleAuth' : {
-        'clientID'      : '547659251063-i9ua0he2m29lpbqr0k1oe6e37r0ummr8.apps.googleusercontent.com',
-        'clientSecret'  : 'HnrzTI4Jy21XIHS747XUbey8',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'clientID'      : '547659251063-pelct1d1qkr8e88b43dcic17ue5dnudm.apps.googleusercontent.com',
+        'clientSecret'  : '4jVXbSZRSrB8qARvxXjim2LJ',
+        'callbackURL'   : '/auth/google/callback'
     }
 };
